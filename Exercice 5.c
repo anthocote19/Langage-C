f6851340be0b28ebdeeec812a59f162f0915ddc6
Anthony Cote <141554336+anthocote19@users.noreply.github.com>
2025-01-13 14:44:52 +0100

    Caesar cipher
    Make the user enter a letter, display its correspondence shifted by 3 (while remaining in the alphabet).

--- Exercice 5.c
+++ Exercice 5.c
@@ -1,1 +1,21 @@
+#include <stdio.h>
+
+int main() {
+    char lettre, lettre_chiffree;
+    printf("Entrez une lettre (a-z ou A-Z) : ");
+    scanf(" %c", &lettre);
+
+    
+    if (lettre >= 'a' && lettre <= 'z') {
+        lettre_chiffree = ((lettre - 'a' + 3) % 26) + 'a';
+    } else if (lettre >= 'A' && lettre <= 'Z') {
+        lettre_chiffree = ((lettre - 'A' + 3) % 26) + 'A';
+    } else {
+        printf("Ce n'est pas une lettre valide.\n");
+        return 1;
+    }
+    printf("Lettre chiffrée : %c\n", lettre_chiffree);
+
+    return 0;
+}
 
