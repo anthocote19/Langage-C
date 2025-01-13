534f7dfbdf957f66180a6cb038270581275c2d27
Anthony Cote <141554336+anthocote19@users.noreply.github.com>
2025-01-13 14:44:25 +0100

    Power
    Write a program that is given two numbers as arguments and displays the result of the first number raised to the power of the second.

--- Exercice 4.c
+++ Exercice 4.c
@@ -1,1 +1,15 @@
+#include <stdio.h>
+#include <math.h>
+
+int main() {
+    double base, puissance, resultat;
+    printf("Entrez la base : ");
+    scanf("%lf", &base);
+    printf("Entrez la puissance : ");
+    scanf("%lf", &puissance);
+    resultat = pow(base, puissance);
+    printf("%.2lf à la puissance %.2lf = %.2lf\n", base, puissance, resultat);
+
+    return 0;
+}
 
