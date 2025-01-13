50011fef907ae115adec328becb3a089b3b23702
Anthony Cote <141554336+anthocote19@users.noreply.github.com>
2025-01-13 14:43:58 +0100

    Truth tables
    Using the logical operators, display the truth table of the implication: x ⇒ y.
    Do the same thing with the equivalence: x ⇔ y.

--- Exercice 3.c
+++ Exercice 3.c
@@ -1,1 +1,26 @@
+#include <stdio.h>
 
+int main() {
+    printf("x | y | x ⇒ y\n");
+    printf("--|---|-------\n");
+    for (int x = 0; x <= 1; x++) {
+        for (int y = 0; y <= 1; y++) {
+            printf("%d | %d |   %d\n", x, y, (!x || y));
+        }
+    }
+    return 0;
+}
+///////////////////////////////////////////////////////////////////////////////
+
+#include <stdio.h>
+
+int main() {
+    printf("x | y | x ⇔ y\n");
+    printf("--|---|-------\n");
+    for (int x = 0; x <= 1; x++) {
+        for (int y = 0; y <= 1; y++) {
+            printf("%d | %d |   %d\n", x, y, (x == y));
+        }
+    }
+    return 0;
+}
