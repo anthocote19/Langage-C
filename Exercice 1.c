e0a4119e7c4eef562b6f4f27ee2f66ffd66f31aa
Anthony Cote <141554336+anthocote19@users.noreply.github.com>
2025-01-13 14:42:54 +0100

    Exchanging two variables
    Write a program to exchange the values of two variables entered by the user.
    Modify the program to perform the same action using the + and - operators.

--- Exercice 1.c
+++ Exercice 1.c
@@ -1,6 +1,38 @@
 #include <stdio.h>
 
 int main() {
-    printf("coucou!\n");
+    int a, b, temp;
+    printf("Entrez la première valeur : ");
+    scanf("%d", &a);
+    printf("Entrez la deuxième valeur : ");
+    scanf("%d", &b);
+    temp = a;
+    a = b;
+    b = temp;
+    printf("Après l'échange :\n");
+    printf("a = %d\n", a);
+    printf("b = %d\n", b);
+
+    return 0;
+}
+
+
+////////////////////////////////////////////////////////////////////////////////////////////
+
+#include <stdio.h>
+
+int main() {
+    int a, b;
+    printf("Entrez la première valeur : ");
+    scanf("%d", &a);
+    printf("Entrez la deuxième valeur : ");
+    scanf("%d", &b);
+    a = a + b;
+    b = a - b;
+    a = a - b;
+    printf("Après l'échange :\n");
+    printf("a = %d\n", a);
+    printf("b = %d\n", b);
+
     return 0;
 }
