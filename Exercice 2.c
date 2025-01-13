6cd2889cd061e9f91bccfc19a0dd941fafae9b1c
Anthony Cote <141554336+anthocote19@users.noreply.github.com>
2025-01-13 14:43:30 +0100

    Time conversion
    Write a program that asks the user for a duration in seconds, and converts it into hours, minutes, seconds: 12334 = 3:25:34.
    Write a program that does the opposite: 3:25:34 = 12334.

--- Exercice 2.c
+++ Exercice 2.c
@@ -1,1 +1,27 @@
+int main() {
+    int secondes, heures, minutes, reste;
+    printf("Entrez une durée en secondes : ");
+    scanf("%d", &secondes);
+    heures = secondes / 3600;
+    reste = secondes % 3600;
+    minutes = reste / 60;
+    reste = reste % 60;
+    printf("Durée convertie : %d:%d:%d\n", heures, minutes, reste);
 
+    return 0;
+}
+
+////////////////////////////////////////////////////////////////////////////////////////////
+
+
+#include <stdio.h>
+
+int main() {
+    int heures, minutes, secondes, total;
+    printf("Entrez la durée (heures:minutes:secondes) : ");
+    scanf("%d:%d:%d", &heures, &minutes, &secondes);
+    total = (heures * 3600) + (minutes * 60) + secondes;
+    printf("Durée en secondes : %d\n", total);
+
+    return 0;
+}
