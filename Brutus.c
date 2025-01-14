3a7c99b6f230f92d04827ea658dc7bb6534d59e0
Anthony Cote <141554336+anthocote19@users.noreply.github.com>
2025-01-14 15:38:55 +0100

    End of Brutus Project
    Create a project named Brutus.
    Add the MD5 library.
    An MD5 is an unsigned char array of 16 packets.
    unsigned char check[17] = {0};
    To get the MD5 in order to crack it, use scanf in hexadecimal mode with a space between the packets.
    scanf("%x %x %x ... ", &check[0], &check[1], ...);
    Ask the user to get the assumed password size.
    int pSize;
    To define the characters to be tested, use an alphabet.
    char *alphabet = "abcdefghijklmnopqrstuvwxyz";
    Create an array of 255 characters to define a password to test.
    Create a digest array of unsigned char to put the generated MD5 of the password from the previous array.
    Create a loop to test all the options.
    md5((unsigned char *) toCheck, pSize, digest);
    Crack 6057f13c496ecf7fd777ceb9e79ae285.
    Crack de9c3cbac5e22a64b3746708837ddd16.

--- Brutus.c
+++ Brutus.c
@@ -1,1 +1,48 @@
+#include <stdio.h>
+#include <string.h>
+#include <openssl/md5.h>
+
+int main() {
+    unsigned char check[16] = {0};  
+    int pSize;                      
+    char *alphabet = "abcdefghijklmnopqrstuvwxyz";  
+    char toCheck[255] = {0};         
+    unsigned char digest[16];        
+    int alphabetSize = strlen(alphabet);  
+
+    printf("Entrez le hash MD5 (16 valeurs hexadécimales séparées par des espaces) :\n");
+    scanf("%x %x %x %x %x %x %x %x %x %x %x %x %x %x %x %x",
+          &check[0], &check[1], &check[2], &check[3], &check[4], &check[5],
+          &check[6], &check[7], &check[8], &check[9], &check[10], &check[11],
+          &check[12], &check[13], &check[14], &check[15]);
+
+    printf("Entrez la taille du mot de passe à tester : ");
+    scanf("%d", &pSize);
+
+    int maxCombinations = 1;
+    for (int i = 0; i < pSize; i++) {
+        maxCombinations *= alphabetSize;  
+    }
+
+    for (int i = 0; i < maxCombinations; i++) {
+        int temp = i;
+        for (int j = 0; j < pSize; j++) {
+            toCheck[j] = alphabet[temp % alphabetSize];  
+            temp /= alphabetSize;  
+        }
+
+        MD5_CTX mdContext;
+        MD5_Init(&mdContext);
+        MD5_Update(&mdContext, toCheck, pSize);
+        MD5_Final(digest, &mdContext);
+
+        if (memcmp(digest, check, 16) == 0) {
+            printf("Mot de passe trouvé : %s\n", toCheck);
+            return 0;  
+        }
+    }
+
+    printf("Mot de passe non trouvé.\n");
+    return 0;
+}
 
