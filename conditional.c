98bb15b10fd127201d7bf8429b3e69e7ce755654
Anthony Cote <141554336+anthocote19@users.noreply.github.com>
2025-01-14 09:27:14 +0100

    Pratique du C plus complexe

---
+++ conditional.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+
+void displayMonthName() {
+    int month;
+
+    printf("Enter a number between 1 and 12: ");
+    if (scanf("%d", &month) != 1) { 
+        printf("Invalid input. Please enter an integer.\n");
+        return;
+    }
+
+    switch (month) {
+        case 1:
+            printf("January\n");
+            break;
+        case 2:
+            printf("February\n");
+            break;
+        case 3:
+            printf("March\n");
+            break;
+        case 4:
+            printf("April\n");
+            break;
+        case 5:
+            printf("May\n");
+            break;
+        case 6:
+            printf("June\n");
+            break;
+        case 7:
+            printf("July\n");
+            break;
+        case 8:
+            printf("August\n");
+            break;
+        case 9:
+            printf("September\n");
+            break;
+        case 10:
+            printf("October\n");
+            break;
+        case 11:
+            printf("November\n");
+            break;
+        case 12:
+            printf("December\n");
+            break;
+        default:
+            printf("Invalid number. Please enter a number between 1 and 12.\n");
+    }
+}
+
+int main() {
+    displayMonthName();
+    return 0;
+}
